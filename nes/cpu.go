02ceb89abc66e98d9b6a7e7803fd718e17fb393a
Greg Bahr <gregsbahr@gmail.com>
2019-09-30 20:38:50 +0100

    added instruction type, started instruction jump table

--- nes/cpu.go
+++ nes/cpu.go
@@ -8,20 +8,31 @@ const (
 	C CPUFlag = (1 << 0) // Carry Bit
 	Z CPUFlag = (1 << 1) // Zero
 	I CPUFlag = (1 << 2) // Disable Interrupts
+	U CPUFlag = (1 << 3) // Decimal (unused by NES, always set to 1)
 	B CPUFlag = (1 << 4) // Break
+	R CPUFlag = (1 << 5) // Reserved (unused by NES, always set to 1)
 	V CPUFlag = (1 << 6) // Overflow
 	N CPUFlag = (1 << 7) // Negative
 )
 
 // CPU is where program execution happens.
 type CPU struct {
-	A   byte   // accumulator
-	X   byte   // index
-	Y   byte   // index
-	PC  uint16 // program counter
-	S   byte   // stack pointer
-	P   byte   // status
-	bus *Bus
+	A          byte   // accumulator
+	X          byte   // index
+	Y          byte   // index
+	PC         uint16 // program counter
+	S          byte   // stack pointer
+	P          byte   // status
+	bus        *Bus
+	cyclesLeft byte // Cycles left in current instruction
+}
+
+// Stores instruction information that makes up an opcode.
+type instruction struct {
+	name           string
+	operation      func() bool
+	addressingMode func() bool
+	cycles         int
 }
 
 // New return a new CPU that is connected to the Bus passed into it.
@@ -38,4 +49,22 @@ func (c *CPU) read(addr uint16) byte {
 
 func (c *CPU) write(addr uint16, data byte) {
 	c.bus.write(addr, data)
+}
+
+func (c *CPU) setFlag(flag CPUFlag, val bool) {
+	if val {
+		c.P |= byte(flag)
+	} else {
+		c.P &= byte(^flag)
+	}
+}
+
+var instructions = map[byte]instruction{
+	0x00: instruction{"BRK", nil, nil, 1}, // 255 more of these ...
+}
+
+func (c *CPU) run(instr instruction) {
+	if instr.addressingMode() && instr.operation() {
+		c.cyclesLeft++
+	}
 }
